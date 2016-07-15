1b6423ed2f8ff5f99ed124a9ea177856b4abc76a
Usa Geek <mayadi1@mail.ccsf.edu>
2016-07-15 12:14:43 -0500

    Initial Commit

---
+++ Pokémon Finder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokémon Finder
+//
+//  Created by Mohamed Ayadi on 7/15/16.
+//  Copyright © 2016 Mohamed Ayadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
