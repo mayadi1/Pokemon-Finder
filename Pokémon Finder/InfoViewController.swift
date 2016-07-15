22f6f871b29acb09b6f02b76c60d585305dc95bc
Mohamed Ayadi <Jouba@Mohameds-MacBook-Air.local>
2016-07-15 16:02:56 -0500

    Fixed Posts

--- Pokémon Finder/InfoViewController.swift
+++ Pokémon Finder/InfoViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import MapKit
 
 class InfoViewController: UIViewController {
     @IBOutlet weak var founderName: UITextField!
 
+    @IBOutlet weak var pokemonName: UITextField!
     @IBOutlet weak var imageView: UIImageView!
     
     var passedName: String?
@@ -21,7 +23,7 @@ class InfoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.founderName.text = self.passedName
-
+        self.imageView.image = UIImage.init(named: self.passedImage!)
     }
 
 
@@ -29,6 +31,28 @@ class InfoViewController: UIViewController {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     @IBAction func navigatebuttonPressed(sender: AnyObject) {
-    }
+        self.openMapForPlace()
+        
+           }
 
+    
+    func openMapForPlace() {
+        
+      
+        
+        let latitute:CLLocationDegrees =  self.passedLat!
+        let longitute:CLLocationDegrees =  self.passedLong!
+        let regionDistance:CLLocationDistance = 10000
+        let coordinates = CLLocationCoordinate2DMake(latitute, longitute)
+        let regionSpan = MKCoordinateRegionMakeWithDistance(coordinates, regionDistance, regionDistance)
+        let options = [
+            MKLaunchOptionsMapCenterKey: NSValue(MKCoordinate: regionSpan.center),
+            MKLaunchOptionsMapSpanKey: NSValue(MKCoordinateSpan: regionSpan.span)
+        ]
+        let placemark = MKPlacemark(coordinate: coordinates, addressDictionary: nil)
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.name = self.passedName
+        mapItem.openInMapsWithLaunchOptions(options)
+        
+    }
 }
